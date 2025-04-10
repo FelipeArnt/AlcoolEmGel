c3ae7a2f2ff2fde804dc6d46e0b6716a5e25eb3d
Felipe Arnt <102182726+FelipeArnt@users.noreply.github.com>
2025-04-10 13:03:13 -0300

    Update AlcoolGel.ino

--- AlcoolGel.ino
+++ AlcoolGel.ino
@@ -1,91 +1,65 @@
 //Bibliotecas
-
-#include Ultrasonic.h
+#include <Ultrasonic.h>
 #include <Servo.h>
 
 //Hardware
-
 #define pinButton 5
 #define pinEcho 7 
 #define pinTrigger 3
 #define pinServo 6
 #define pinLED 4
 
-
 //Objetos e variaveis
-
 Servo servo; //Motor
 Ultrasonic ultrasonic(pinTrigger, pinEcho); 
 
 boolean tmp = 0;
 volatile uint16_t tmp2 = 0;
-volatile byte cicles;
-volatile byte i ;
-
-//Funcoes
-uint16_t readSensorandCompare();
-void alcoolT(byte maxAngle, byte minAngle);
-void interface();
+volatile byte cicles = 1;
+volatile byte i = 0;
 
 void setup(){
   Serial.begin(9600);
   servo.attach(pinServo);
-  servo.write(100); //setando a posição inicial do motor;
-
-  //Ent e Saida digital
-
+  servo.write(100); //posição inicial
+  
   pinMode(pinLED, OUTPUT);
   pinMode(pinButton, INPUT_PULLUP);
-
-  //Interrupção Externa (ISR0)
-
+  
   attachInterrupt(digitalPinToInterrupt(pinButton), ISR0, FALLING);
 }
 
 void ISR0(){
   tmp2++;
+  if(tmp2 > 2) tmp2 = 0;
   
-  if(tmp2 > 2)tmp2 = 0;
-  
-  if(tmp2 == 0) cicles = 1;
-  if(tmp2 == 1) cicles = 2;
-  if(tmp2 == 2) cicles = 3;
-  
-  i = 0; 
+  cicles = tmp2 + 1; // 1, 2 ou 3 ciclos
+  i = 0;
 }
-
-
-void loop(){
-  readSensorandCompare();
-  interface();
-
-  delay(400);
-}
-
-//Sensor Ultrassônico
 
 uint16_t readSensorandCompare(){
-
-  uint16_t distCM = ultrasonic.convert(ultrasonic.timing(), Ultrasonic :: CM); //distância
-  Serial.print(tmp2 : );
+  uint16_t distCM = ultrasonic.convert(ultrasonic.timing(), Ultrasonic::CM);
+  
+  Serial.print("tmp2: ");
   Serial.print(tmp2);
-  Serial.print( Cicles :);
+  Serial.print(" Cicles: ");
   Serial.print(cicles);
-  Serial.print(distCM :);
-  Serial.print(distCM);
+  Serial.print(" distCM: ");
+  Serial.println(distCM);
 
-
-//Distancia menor de 10 ativa o alcool em gel!
-
-if (distCM < 10 && tmp == 0 && tmp2 == 0){alcoolT(100, 80);} 
-
-if (distCM < 10 && tmp == 0 && tmp2 == 1){alcoolT(100, 70);}
-
-if(distCM < 10 && tmp == 0 && tmp2 == 2){alcoolT(100, 60);}
-
-if(distCM > 16){tmp = 0;}
-
-return distCM;
+  if(distCM < 10 && tmp == 0){
+    switch(tmp2){
+      case 0: alcoolT(100, 80); break;
+      case 1: alcoolT(100, 70); break;
+      case 2: alcoolT(100, 60); break;
+    }
+  }
+  
+  if(distCM > 16){
+    tmp = 0;
+  }
+  
+  return distCM;
 }
 
 
